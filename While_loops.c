8cb648ca7388e693ec1b21b168883ae18f7ba2c1
Mr.Sonsare <135420501+mrsonsare@users.noreply.github.com>
2023-07-13 22:49:08 +0530

    Create While_loops.c

---
+++ While_loops.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+  int i;
+  i = 1;
+  while(i<10){
+  printf("Hello World\t\t\t\t Hello World\n");
+    i++;
+    continue;
+  }
+  i=1;
+  while(i<10){
+  printf("Hello World\t");
+    i++;
+  }
+  i =1;
+  while(i<10){
+  printf("\nHello World\t\t\t\t Hello World\n");
+    i++;
+  }
+  return 0;
+}
